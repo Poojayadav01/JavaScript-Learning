ff015e834ca251869d52a655e740d2614b233a64
Poojayadav01 <86109599+Poojayadav01@users.noreply.github.com>
2025-02-21 12:29:45 +0000

    datatypes

---
+++ javas/datatype_notes.js
@@ -0,0 +1,24 @@
+//console.log(2>1)
+/* console.log(2=>1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)*/
+
+
+/* how the data stored in memory and accessed, on the basic there are two categories.
+1.Primitive - all these primitive types  are call by value- 
+there are 7 types -
+1> Number
+2>String
+3> Boolean
+4>BigInt
+5>Null
+6>Undefined
+7>Symbol
+
+
+===================================
+2. Non-Primitive(refernce type)- Array, Object, Functions
+
+** javaScript is a dynamically typed language */
+
