769cb944cd53498d5748f4ed3bd291de4b23513b
Poojayadav01 <86109599+Poojayadav01@users.noreply.github.com>
2025-02-24 08:33:21 +0000

    function

--- javas/functions/01function.js
+++ javas/functions/01function.js
@@ -14,6 +14,27 @@ function userlogin(username='poo'){
 
 //console.log(userlogin('kiran'))
 
-function calculateCartPrice(){
-    
+function calculateCartPrice(...num){
+    return num
 }
+
+//console.log(calculateCartPrice(200, 100, 900))
+
+const user={
+    username:"pooj",
+    email:'poo@gmail.com'
+}
+
+function handleUser(anyobject){
+    const {username:user ,email}=anyobject
+    console.log(`My name is ${user} and my email is  ${email}`)
+}
+
+//handleUser(user)
+
+const num=[1,4,5,6]
+
+function printSecondElement(num1){
+    return num1[1]
+} 
+console.log(printSecondElement(num))
