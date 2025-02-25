320de7f1a3f76188122777320ee6bdca68e01572
Poojayadav01 <86109599+Poojayadav01@users.noreply.github.com>
2025-02-25 17:17:43 +0000

    iife

---
+++ javas/functions/iife.js
@@ -0,0 +1,11 @@
+//iife-- Immediately Invoked Function Expresssion
+(function datadaseC(){//named iife
+    console.log("DB Connected");
+    
+})();
+
+((name)=>{
+    console.log(`hii ${name} your db connected too`);
+    
+})('pooja')
+
