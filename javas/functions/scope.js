98e17ad187bea2d9991d94c3d7403c076310bb27
Poojayadav01 <86109599+Poojayadav01@users.noreply.github.com>
2025-02-25 04:35:40 +0000

    scope

---
+++ javas/functions/scope.js
@@ -0,0 +1,26 @@
+function first(){
+    const user="poo"
+    function second(){
+        const website="YouTube"
+        console.log(user)
+    }
+    //console.log(website)
+    second()
+}
+
+//first()
+
+if(true){
+    const username="poo"
+    if(username=="poo"){
+        const website="YouTube"
+        console.log(username+website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(username);
+
+
+//==============Interesting=======
